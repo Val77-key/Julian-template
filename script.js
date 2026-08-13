b0a6873ec5d4470797ad533d480f353c66d0fab2
Val77-key <261683494+Val77-key@users.noreply.github.com>
2026-08-12 19:55:17 -0700

    mobile nav active states

--- script.js
+++ script.js
@@ -84,3 +84,13 @@ openBtn.addEventListener('click', () => {
 closeBtn.addEventListener('click', () => {
     closeMobileNav();
 });
+
+
+const navLinks = document.querySelectorAll('.link');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        navLinks.forEach(link => link.classList.remove('active'));
+        link.classList.add('active');
+    });
+});
